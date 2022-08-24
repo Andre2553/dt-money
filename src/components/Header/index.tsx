2e208b48d4bd5473252fffc48cbea50499603572
Andre Rodrigues Lima <andrezaga9@hotmail.com>
2022-08-24 12:25:43 +1000

    modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-import logoImg from '../../assets/logo.svg'
-export function Header(){
-return (
-   <HeaderContainer>
+import logoImg from "../../assets/logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransactionModal";
+export function Header() {
+  return (
+    <HeaderContainer>
       <HeaderContent>
-         <img src={logoImg} alt="logo" />
-         <NewTransactionButton>New Transaction</NewTransactionButton>
+        <img src={logoImg} alt="logo" />
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>New Transaction</NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransactionModal/>
+        </Dialog.Root>
       </HeaderContent>
-   </HeaderContainer>
-   )
+    </HeaderContainer>
+  );
 }
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,70 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import { PriceHighlight, TransactionContainer, TransactionTable } from "./styles";
+
+interface Transaction{
+   id: number;
+   description: string;
+   type: 'income' | 'outcome';
+   price: number;
+   category: string;
+   createAt: string;
+}
 
 export function Transactions() {
-  return (
+   const [transactions, setTransactions] = useState<Transaction[]>([])
 
-   <Summary/>
+   async function loadTransactions(){
+      const response = await fetch('http://localhost:3333/transactions')
+      const data = await response.json();
+      setTransactions(data);
+   }
+
+   useEffect(()=>{
+      loadTransactions();
+   },[])
+  return (
+    <div>
+      <Summary />
+      <TransactionContainer>
+         <SearchForm/>
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td width="50%">AAAA</td>
+              <td>
+                <PriceHighlight variant="income">$213,312.33</PriceHighlight>
+              </td>
+              <td>Income</td>
+              <td>24/08/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">AAAA</td>
+              <td><PriceHighlight variant="outcome">$213,312.33</PriceHighlight></td>
+              <td>Spends</td>
+              <td>24/08/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">AAAA</td>
+              <td>
+                <PriceHighlight variant="income">$213,312.33</PriceHighlight>
+              </td>
+              <td>Income</td>
+              <td>24/08/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">AAAA</td>
+              <td>
+                <PriceHighlight variant="income">$213,312.33</PriceHighlight>
+              </td>
+              <td>Income</td>
+              <td>24/08/2022</td>
+            </tr>
+          </tbody>
+        </TransactionTable>
+      </TransactionContainer>
+    </div>
   );
 }
